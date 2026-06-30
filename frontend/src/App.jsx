8b0bf97da87cc475f44d896ba97de57b48cdb983
João Victor Arantes Oliveira <ojoao830@gmail.com>
2026-06-30 12:46:35 -0300

    feat: define estilos padrões e cria login & register (sem funcionalidades)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import './App.css'
+import Login from './pages/Auth/Login'
+import Register from './pages/Auth/Register'
+
 
 function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/Auth/Login.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Auth.css';
+import logo from '../../images/logo_auth.png';
+
+const Login = () => {
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+
+        <h1 className="auth-title">Seja bem vindo!</h1>
+        <p className="auth-subtitle">Faça login para fazer seus pedidos</p>
+
+        <form className="auth-form">
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Digite seu e-mail" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="senha">Senha</label>
+            <input type="password" placeholder="Digite sua senha" />
+          </div>
+
+          <div className="auth-links">
+            <Link to="/register" className="link-criar-conta">Criar conta</Link>
+          </div>
+
+          <button type="submit" className="btn-primary">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
