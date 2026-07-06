11d7baf9fb042f318e2c9a3c9dab661780011737
Wellington Costa Grilo Pereira <wellingtoncostagrilo2018@gmail.com>
2026-07-06 20:12:06 -0300

    feat: adiciona rotas de carrinho ao index

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const authRoutes = require('./authRoutes');
-const clienteRoutes = require('./clienteRoutes');
 const produtoRoutes = require('./produtoRoutes');
 const pedidoRoutes = require('./pedidoRoutes');
 const adminRoutes = require('./adminRoutes');
+const cartRoutes = require('./cartRoutes');
 
 const router = express.Router();
 
 router.get("/health", (req, res) => {res.send("O server esta online");}); 
 
 router.use(authRoutes);
-router.use(clienteRoutes);
 router.use(produtoRoutes);
 router.use(pedidoRoutes);
 router.use(adminRoutes);
+router.use(cartRoutes);
 
 router.use((req, res) => {
   res.status(404).json({ success: false, message: 'Rota não encontrada.' });
